37959b8c57a0964c444aabfaba350ad68cf8c9de
genesisMoran <contactgmmr@gmail.com>
2018-08-04 16:38:31 -0400

    commit 1: started db: seed

--- db/schema.sql
+++ db/schema.sql
@@ -4,16 +4,41 @@ CREATE DATABASE unfiltered_experiment_db;
 
 \c unfiltered_experiment_db;
 
-DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS responses CASCADE;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS responses;
+DROP TABLE IF EXISTS emotions;
 
 CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    impression_1 TEXT NOT NULL,
-    impression_2 VARCHAR(255) NOT NULL,
-    impression_3 VARCHAR(255) NOT NULL
+            id SERIAL PRIMARY KEY,
+          name VARCHAR(50),
+           age INTEGER
 );
 
 CREATE TABLE responses (
+            id SERIAL PRIMARY KEY,
+  impression_1 TEXT,
+  impression_2 TEXT,
+  impression_3 INTEGER REFERENCES emotions(id),
+      users_id INTEGER REFERENCES users(id)
+);
 
+CREATE TABLE emotions (
+            id SERIAL PRIMARY KEY,
+     emotion_1 VARCHAR(20),
+     emotion_2 VARCHAR(20),
+     emotion_3 VARCHAR(20),
+     emotion_4 VARCHAR(20),
+     emotion_5 VARCHAR(20),
+     emotion_6 VARCHAR(20),
+     emotion_7 VARCHAR(20),
+     emotion_8 VARCHAR(20),
+     emotion_9 VARCHAR(20),
+    emotion_10 VARCHAR(20),
+    emotion_11 VARCHAR(20),
+    emotion_12 VARCHAR(20),
+    emotion_13 VARCHAR(20),
+    emotion_14 VARCHAR(20),
+    emotion_15 VARCHAR(20),
+    emotion_16 VARCHAR(20),
+    emotion_17 VARCHAR(20)    
 );
---
+++ db/seed.sql
@@ -0,0 +1,16 @@
+\c unfiltered_experiment_db;
+
+INSERT INTO users 
+(id, name, age)
+VALUES 
+(DEFAULT, 'Genesis Moran', 25);
+
+INSERT INTO responses
+(id, impression_1, impression_2, impression_3, users_id)
+VALUES
+(DEFAULT, 'This is a test', 'This is a test too', DEFAULT, DEFAULT);
+
+INSERT INTO emotions
+(id, )
+
+
